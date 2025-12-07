55a31ebf74430d0b044638d4f5840f88fbfd64dc
Miguel Angel Suarez <miguel.buny@gmail.com>
2025-12-06 21:28:44 -0600

    Listo introduccion a React

---
+++ 55-cotizador-prestamos-react/src/App.jsx
@@ -0,0 +1,129 @@
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Button from './components/Button'
+import { formatearDinero, calcularTotalPagar } from './helpers';
+
+function App() {
+
+  const [cantidad, setCantidad] = useState(10000);
+  const [meses, setMeses] = useState(6); //select cantidad de meses
+  const [total, setTotal] = useState(calcularTotalPagar(0));
+  const [pago, setPago]=useState(0);
+
+  //total a pagar 
+  useEffect(() => {
+    const resultadoTotalPagar = calcularTotalPagar(cantidad, meses);
+    setTotal(resultadoTotalPagar);
+  }, [cantidad, meses]);
+
+  //total a pagar por meses
+  useEffect(()=>{
+    setPago(total/meses);
+  }, [total]);
+  //variables fijas
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
+
+  //función setear cantidad
+  function handleChange(e) {
+    setCantidad(Number(e.target.value));
+  }
+
+  //restar de 100 en 100 al picar al boton de "-"
+  function handleClickRestar(e) {
+    //tomara la cantidad actual (state) y le restara el step (100)
+    const valor = cantidad - STEP;
+
+    if (valor < MIN) {
+      alert('Minimo es 0');
+      return;
+    }
+    //modifica el state al final y renderizará la vista
+    setCantidad(valor);
+
+  }
+  function handleClickSumar(e) {
+    //tomara la cantidad actual (state) y le restara el step (100)
+    const valor = cantidad + STEP;
+
+    if (valor > MAX) {
+      alert(`Maximo es :${MAX}`);
+      return;
+    }
+    //modifica el state al final y renderizará la vista
+    setCantidad(valor);
+
+  }
+
+
+
+  return (
+
+    <div className='my-20 max-w-lg mx-auto bg-white shadow p-10'>
+      <Header />
+
+      {/*Botones de incremento y decremento */}
+      <div className="flex justify-between my-6">
+
+        <Button
+          //los componente le puedes enviar diferentes props
+          operador='-'
+          fn={handleClickRestar}
+        />
+
+        <Button
+          operador='+'
+          fn={handleClickSumar}
+        />
+
+      </div>
+
+
+      <input type="range"
+        className='w-full bg-gray-200 h-6 accent-lime-500 hover:accent-lime-600'
+        onChange={handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={cantidad}
+      />
+      <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
+        {formatearDinero(cantidad)} MXN
+      </p>
+
+
+      <h2 className='text-2xl font-extrabold text-gray-500 text-center '>
+        Elige un <span className='text-indigo-600'>plazo </span> a pagar
+      </h2>
+
+      <select
+        className='mt-5 w-full p-2 bg-white border border-gray-300 rounded-lg text-center text-xl font-bold text-gray-500'
+        value={meses}
+        onChange={e => setMeses( parseInt(e.target.value))}
+      >
+        <option value="6">6 meses</option>
+        <option value="12">12 meses</option>
+        <option value="24">24 meses</option>
+      </select>
+
+
+      <div className="mt-5 space-y-3 bg-gray-50 p-5">
+        <h2 className='text-2xl font-extrabold text-gray-500 text-center '>
+          Resumen <span className='text-indigo-600'>de pagos </span>
+        </h2>
+
+
+        <p className="text-xl text-gray-500 text-center font-bold" >{meses} Meses</p>
+        <p className="text-xl text-gray-500 text-center font-bold" >{formatearDinero(total)} Total a pagar</p>
+        <p className="text-xl text-gray-500 text-center font-bold" >{formatearDinero(pago)} mensuales</p>
+      </div>
+
+
+    </div>
+
+  )
+}
+
+export default App
---
+++ 55-cotizador-prestamos-react/src/helpers/index.js
@@ -0,0 +1,37 @@
+function formatearDinero(valor){
+    //API de internalización en JS
+    const formater =new Intl.NumberFormat('es-MX',{
+        style:'currency',
+        currency:'MXN'
+    });
+    return formater.format(valor);
+}
+
+const calcularTotalPagar=(cantidad, plazo)=>{
+    let total;
+    //mientras mayor es la cantidad, menor son los intereses
+    if (cantidad<5000) {
+        total=cantidad*1.5;
+    }else if(cantidad >=5000 && cantidad <10000){
+        total=cantidad*1.4;
+    }else if(cantidad >=10000 && cantidad <15000){
+        total=cantidad*1.3;
+    }else{
+        total=cantidad*1.2;
+    }
+
+    //plazo - más plazo , mayor interes
+    if(plazo === 6){
+        total *=1.1;
+    }else if(plazo ===12){
+        total *=1.2;
+    }else{
+        total*=1.3
+    }
+    return total;
+}
+
+export {
+    formatearDinero,
+    calcularTotalPagar
+}
